c2b24b91a54f1978e6eb1d48d32545aa24e3c2db
DialecticalLaw <d.shmuratkin@mail.ru>
2024-05-09 23:09:52 +0300

    feat: change Button design

--- src/components/univComponents/Button/Button.tsx
+++ src/components/univComponents/Button/Button.tsx
@@ -4,8 +4,7 @@ import styles from './Button.module.css';
 export function Button({ children, type, onClick }: ButtonProps) {
   return (
     <button className={styles.button} type={type} onClick={onClick}>
-      {children}
-      <span className={styles.underline}>_</span>
+      <p className={styles.buttonText}>{children}</p>
     </button>
   );
 }
