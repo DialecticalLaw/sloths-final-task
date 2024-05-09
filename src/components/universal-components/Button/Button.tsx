006477e23c72c218775bbf8d300a0f786b977d31
DialecticalLaw <d.shmuratkin@mail.ru>
2024-05-09 21:04:12 +0300

    fix: add onClick prop for Button component

--- src/components/universal-components/Button/Button.tsx
+++ src/components/universal-components/Button/Button.tsx
@@ -1,8 +1,16 @@
 import styles from './Button.module.css';
 
-export function Button({ children, type }: { children: string; type: 'submit' | 'reset' | 'button' }) {
+export function Button({
+  children,
+  type,
+  onClick
+}: {
+  children: string;
+  type: 'submit' | 'reset' | 'button';
+  onClick?: () => void;
+}) {
   return (
-    <button className={styles.button} type={type}>
+    <button className={styles.button} type={type} onClick={onClick}>
       {children}
       <span className={styles.underline}>_</span>
     </button>
