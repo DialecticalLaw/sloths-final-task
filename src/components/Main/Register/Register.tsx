7200219e20e54a074c33605afd914e1468889398
DialecticalLaw <d.shmuratkin@mail.ru>
2024-05-09 11:32:42 +0300

    fix: remove the link to the login page

--- src/components/Main/Register/Register.tsx
+++ src/components/Main/Register/Register.tsx
@@ -9,7 +9,6 @@ import video from '../../../assets/video/starry-sky.webm';
 import { Planets } from './Planets/Planets';
 import { date, object, string } from 'yup';
 import { CountrySelect } from './CountrySelect/CountrySelect';
-import { Link } from '../../universal-components/CustomForm/Link/Link';
 
 export interface StringObj {
   [key: string]: string;
@@ -127,7 +126,6 @@ export function Register() {
 
               <Planets name={'planet'}></Planets>
               <Button type="submit">Register</Button>
-              <Link text="Already have an account?">Log in</Link>
             </>
           </CustomForm>
         )}
