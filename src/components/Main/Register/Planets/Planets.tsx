5cac3fc0c5c67e504f2dc0a76f061d423d66d18d
DialecticalLaw <d.shmuratkin@mail.ru>
2024-05-09 20:41:48 +0300

    refactor: remove unnecessary closing tags

--- src/components/Main/Register/Planets/Planets.tsx
+++ src/components/Main/Register/Planets/Planets.tsx
@@ -6,9 +6,9 @@ export function Planets({ name }: { name: string }) {
     <>
       <h2 className={styles.title}>Select a planet</h2>
       <div className={styles.planets}>
-        <Field className={`${styles.planet} ${styles.venus}`} type="radio" name={name} value="venus"></Field>
-        <Field className={`${styles.planet} ${styles.earth}`} type="radio" name={name} value="earth"></Field>
-        <Field className={`${styles.planet} ${styles.mars}`} type="radio" name={name} value="mars"></Field>
+        <Field className={`${styles.planet} ${styles.venus}`} type="radio" name={name} value="venus" />
+        <Field className={`${styles.planet} ${styles.earth}`} type="radio" name={name} value="earth" />
+        <Field className={`${styles.planet} ${styles.mars}`} type="radio" name={name} value="mars" />
       </div>
     </>
   );
