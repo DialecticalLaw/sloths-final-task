fa2e0fee762a2cbc16e84215a0b7495daa57eb6d
Natalia Bandurova <natacsp@mail.ru>
2024-05-11 17:23:41 +0300

    fix: fix validation message

--- src/components/Main/validationSchemes.ts
+++ src/components/Main/validationSchemes.ts
@@ -3,7 +3,7 @@ import { date, object, string } from 'yup';
 const email = string()
   .email('Invalid email')
   .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Invalid email')
-  .required('Required field');
+  .required('Required field!');
 
 const password = string()
   .min(8, "It's too short! Minimum of 8 characters")
@@ -11,8 +11,8 @@ const password = string()
   .matches(/.*[a-z].*/, 'At least 1 lowercase letter')
   .matches(/.*[0-9].*/, 'At least 1 number')
   .matches(/[!@#$%^&*]/, 'At least 1 special character !@#$%^&*')
-  .required('Required')
-  .test('no-whitespace', 'Must not contain leading or trailing whitespace', (value) => {
+  .required('Required field!')
+  .test('no-whitespace', 'No leading or trailing whitespace', (value) => {
     return !(/^\s/.test(value) || /\s$/.test(value));
   });
 
@@ -22,24 +22,24 @@ export const RegisterSchema = object().shape({
   firstName: string()
     .min(1, 'Too short!')
     .matches(/^[a-zA-Z]+$/, 'No special characters or numbers')
-    .required('Required'),
+    .required('Required field!'),
   lastName: string()
     .min(1, 'Too short!')
     .matches(/^[a-zA-Z]+$/, 'No special characters or numbers')
-    .required('Required'),
+    .required('Required field!'),
   dateOfBirth: date()
-    .required('Required!')
+    .required('Required field!')
     .test('dateOfBirth', '13 years old or older', function (value: Date | undefined) {
       if (!value) return false;
       return new Date().getFullYear() - value.getFullYear() >= 13;
     }),
-  street: string().min(1, 'Too short!').required('Required'),
+  street: string().min(1, 'Too short!').required('Required field!'),
   city: string()
     .min(1, 'Too short!')
     .matches(/^[a-zA-Z]+$/, 'No special characters or numbers')
-    .required('Required'),
+    .required('Required field!'),
   postalCode: string()
-    .required('Required')
+    .required('Required field!')
     .length(6, '6 numbers are needed')
     .matches(/^[0-9]+$/, 'Only numbers are allowed')
 });
