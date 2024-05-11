675a9c00461f59ecb403a71b76ffc6915bab0b74
Natalia Bandurova <natacsp@mail.ru>
2024-05-11 17:36:49 +0300

    refactor: rm comments

--- src/components/univComponents/CustomForm/Input/Input.tsx
+++ src/components/univComponents/CustomForm/Input/Input.tsx
@@ -4,16 +4,6 @@ import type { InputProps } from '../../UnivComponents.interfaces';
 import { PasswordButton } from '../PasswordButton/PasswordButton';
 import styles from './Input.module.css';
 
-// export function Input({ name, type, placeholder, children }: InputProps) {
-//   return (
-//     <label className={styles.label}>
-//       <Field className={styles.field} name={name} type={type} placeholder={placeholder} />
-//       <div className={styles.line} />
-//       {children}
-//     </label>
-//   );
-// }
-
 export function Input({ name, type, placeholder, children }: InputProps) {
   const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false);
 
--- src/components/univComponents/CustomForm/Title/Title.tsx
+++ src/components/univComponents/CustomForm/Title/Title.tsx
@@ -1,14 +1,5 @@
 import styles from './Title.module.css';
 
-// export function Title({ children }: { children: string }) {
-//   return (
-//     <>
-//       <h2 className={styles.titleAddition}>Welcome to the future</h2>
-//       <h1 className={styles.titleMain}>{children}</h1>
-//     </>
-//   );
-// }
-
 export function Title({ mainText, additionText }: { mainText: string; additionText: string }) {
   return (
     <>
