340de8b380900701d7794a0bdc2ce73ac33998d5
ValeriiaKuz <lerabukareva@icloud.com>
2024-05-12 13:27:01 +0300

    feat: change main layout for site

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
 import { Main } from './components/Main/Main';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <>
       <Header />
+      <Sidebar />
       <Main>
         <Outlet />
       </Main>
