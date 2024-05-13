309f88af9274965e1b46dc8cfb89f74bef371362
ValeriiaKuz <lerabukareva@icloud.com>
2024-05-13 12:35:41 +0300

    fix: fix import of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { Main } from './components/Main/Main';
-import Sidebar from './components/Sidebar/Sidebar';
 import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 export function App() {
   return (
