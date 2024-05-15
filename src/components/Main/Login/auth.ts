36e4fac9c5fcb645a09d238edaed50d96d9fec26
DialecticalLaw <d.shmuratkin@mail.ru>
2024-05-15 13:29:59 +0300

    Merge branch 'release/sprint2' of github.com:DialecticalLaw/sloths-final-task into feat/RSS-ECOMM-2_15

---
+++ src/components/Main/Login/auth.ts
@@ -0,0 +1,76 @@
+import type { LoginValues } from '../Main.interfaces';
+import type { AppDispatch } from '../../../store/store';
+import { showToast } from '../../../helpers/showToast';
+import { setCustomer } from '../../../store/slices/customer-slice';
+import { loginCustomer } from '../../../api/customers/loginCustomer';
+import type { FormikState } from 'formik';
+import type { RegisterValues } from '../Main.interfaces';
+import type { CustomerBody } from '../../../api/api.interfaces';
+
+export const login = async (
+  values: LoginValues,
+  dispatch: AppDispatch,
+  resetForm?: (nextState?: Partial<FormikState<LoginValues>> | undefined) => void
+): Promise<void> => {
+  try {
+    await loginCustomer(values).then(({ body }) => {
+      showToast({
+        text: 'Successful login!',
+        type: 'success'
+      });
+      if (resetForm) {
+        resetForm();
+      }
+      dispatch(setCustomer(body.customer));
+    });
+  } catch (error) {
+    showToast({
+      text: 'Incorrect email or password. Please try again!',
+      type: 'error'
+    });
+  }
+};
+
+export const formatCustomerData = (values: RegisterValues): CustomerBody => {
+  const shippingIndex = 0;
+  const billingIndex = 1;
+
+  const customerBody: CustomerBody = {
+    email: values.email,
+    password: values.password,
+    firstName: values.firstName,
+    lastName: values.lastName,
+    dateOfBirth: values.dateOfBirth,
+    addresses: [
+      {
+        city: values.shipping.city,
+        country: values.shipping.country === 'Russia' ? 'RU' : 'BY',
+        postalCode: values.shipping.postalCode,
+        streetName: values.shipping.street
+      }
+    ],
+    shippingAddresses: [shippingIndex],
+    billingAddresses: values.shipping.isSameAddress ? [shippingIndex] : [billingIndex]
+  };
+
+  if (values.shipping.isSameAddress && values.shipping.isDefault) {
+    customerBody.defaultShippingAddress = shippingIndex;
+    customerBody.defaultBillingAddress = shippingIndex;
+  } else if (!values.shipping.isSameAddress) {
+    customerBody.addresses.push({
+      city: values.billing.city,
+      country: values.shipping.country === 'Russia' ? 'RU' : 'BY',
+      postalCode: values.billing.postalCode,
+      streetName: values.billing.street
+    });
+
+    if (values.shipping.isDefault) {
+      customerBody.defaultShippingAddress = shippingIndex;
+    }
+    if (values.billing.isDefault) {
+      customerBody.defaultBillingAddress = billingIndex;
+    }
+  }
+
+  return customerBody;
+};
--- src/components/Main/Register/Register.tsx
+++ src/components/Main/Register/Register.tsx
@@ -9,8 +9,9 @@ import type { RegisterValues } from '../Main.interfaces';
 import { CustomLink } from '../../univComponents/CustomForm/CustomLink/CustomLink';
 import { showToast } from '../../../helpers/showToast';
 import { createCustomer } from '../../../api/customers/createCustomer';
-import type { CustomerBody } from '../../../api/api.interfaces';
+import { useAppDispatch } from '../../../store/hooks';
 import { Address } from '../../univComponents/CustomForm/Address/Address';
+import { formatCustomerData, login } from '../Login/auth';
 
 const initialValues: RegisterValues = {
   email: '',
@@ -35,62 +36,23 @@ const initialValues: RegisterValues = {
   }
 };
 
-const submitCustomerData = (values: RegisterValues) => {
-  const shippingIndex = 0;
-  const billingIndex = 1;
-
-  const customerBody: CustomerBody = {
-    email: values.email,
-    password: values.password,
-    firstName: values.firstName,
-    lastName: values.lastName,
-    dateOfBirth: values.dateOfBirth,
-    addresses: [
-      {
-        city: values.shipping.city,
-        country: values.shipping.country === 'Russia' ? 'RU' : 'BY',
-        postalCode: values.shipping.postalCode,
-        streetName: values.shipping.street
-      }
-    ],
-    shippingAddresses: [shippingIndex],
-    billingAddresses: values.shipping.isSameAddress ? [shippingIndex] : [billingIndex]
-  };
-
-  if (values.shipping.isSameAddress && values.shipping.isDefault) {
-    customerBody.defaultShippingAddress = shippingIndex;
-    customerBody.defaultBillingAddress = shippingIndex;
-  } else if (!values.shipping.isSameAddress) {
-    customerBody.addresses.push({
-      city: values.billing.city,
-      country: values.shipping.country === 'Russia' ? 'RU' : 'BY',
-      postalCode: values.billing.postalCode,
-      streetName: values.billing.street
-    });
-
-    if (values.shipping.isDefault) {
-      customerBody.defaultShippingAddress = shippingIndex;
-    }
-    if (values.billing.isDefault) {
-      customerBody.defaultBillingAddress = billingIndex;
-    }
-  }
-
-  return createCustomer(customerBody);
-};
-
 export function Register() {
+  const dispatch = useAppDispatch();
   return (
     <>
       <Formik
         initialValues={initialValues}
         validationSchema={RegisterSchema}
-        onSubmit={async (values) => {
+        onSubmit={async (values: RegisterValues) => {
+          const customerPromise = createCustomer(formatCustomerData(values));
           showToast({
-            promise: submitCustomerData(values),
+            promise: customerPromise,
             pending: 'Processing...',
             success: 'Successful registration!',
             error: 'error'
+          });
+          customerPromise.then(() => {
+            login({ email: values.email, password: values.password }, dispatch);
           });
         }}
       >
@@ -98,22 +60,17 @@ export function Register() {
           <>
             <Title mainText={'Register'} additionText={'Welcome to the future'}></Title>
             <Input name={'email'} type="email" placeholder="Email"></Input>
-
             <Input name={'password'} type="password" placeholder="Password"></Input>
-
             <div className={styles.inputsGroup}>
               <Input name={'firstName'} type="text" placeholder="Name"></Input>
-
               <Input name={'lastName'} type="text" placeholder="Surname"></Input>
             </div>
-
             <Input name={'dateOfBirth'} type="date" placeholder="Date of birth"></Input>
 
             <Address name="shipping" />
             <Address name="billing" />
 
             <Button type="submit">Register</Button>
-
             <CustomLink text="Already have an account?" to="/login">
               Login
             </CustomLink>
