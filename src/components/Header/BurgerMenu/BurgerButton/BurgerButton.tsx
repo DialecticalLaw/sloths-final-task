6ae0e2014c209fee5935f1b357079aa4bc819559
Natalia Bandurova <natacsp@mail.ru>
2024-05-19 12:55:14 +0300

    refactor: delete unused files

--- src/components/Header/BurgerMenu/BurgerButton/BurgerButton.tsx
+++ src/components/Header/BurgerMenu/BurgerButton/BurgerButton.tsx
@@ -2,26 +2,6 @@ import visible from './../../../../assets/img/visible.svg';
 import noVisible from './../../../../assets/img/noVisible.svg';
 import styles from './BurgerButton.module.css';
 
-// interface ClickHandlerInterface {
-//   clickHandler?: (open: boolean) => void;
-// }
-
-// export const BurgerCloseBtn = ({ clickHandler }: ClickHandlerInterface) => {
-//   return (
-//     <button
-//       type="button"
-//       className="-m-2.5 rounded-md p-2.5 text-secondary"
-//       onClick={() => {
-//         if (clickHandler === undefined) return;
-//         clickHandler(false);
-//       }}
-//     >
-//       <span className="sr-only">Close menu</span>
-//       <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-//     </button>
-//   );
-// };
-
 interface BurgerButtonProps {
   toggleMenuOpen: () => void;
   menuOpen: boolean;
