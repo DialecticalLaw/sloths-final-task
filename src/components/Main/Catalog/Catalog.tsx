3be56877ff7cc5eaeb325a000708b6cda43933ff
ValeriiaKuz <lerabukareva@icloud.com>
2024-05-23 23:19:38 +0300

    feat: add catalog and product card demo interface

--- src/components/Main/Catalog/Catalog.tsx
+++ src/components/Main/Catalog/Catalog.tsx
@@ -2,20 +2,30 @@ import { useEffect } from 'react';
 import { getProducts } from '../../../api/products/getProducts';
 import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 import { Loader } from '../Loader/Loader';
+import { ProductCard } from './ProductCard/ProductCard';
+import type { productsSliceState } from '../../../store/slices/products-slice';
+import styles from './Catalog.module.css';
 
 export function Catalog() {
   const dispatch = useAppDispatch();
-  const { isProductsLoading } = useAppSelector((state) => state.products_slice);
-
+  const { isProductsLoading, products }: productsSliceState = useAppSelector((state) => state.products_slice);
+  const { planet } = useAppSelector((state) => state.planet_slice);
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    if (planet) {
+      dispatch(getProducts(planet));
+    }
+  }, [dispatch, planet]);
 
   return isProductsLoading ? (
     <Loader />
   ) : (
     <>
       <h1>Каталог</h1>
+      <section className={styles.cards_wrapper}>
+        {products.map((productData, index) => (
+          <ProductCard product={productData} key={index} />
+        ))}
+      </section>
     </>
   );
 }
