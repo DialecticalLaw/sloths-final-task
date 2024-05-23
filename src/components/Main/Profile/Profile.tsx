936541fff72590bcc773d5e0fc62ea1aaedf125c
DialecticalLaw <d.shmuratkin@mail.ru>
2024-05-23 18:39:01 +0300

    feat: display user data in the profile

--- src/components/Main/Profile/Profile.tsx
+++ src/components/Main/Profile/Profile.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useAppSelector } from '../../../store/hooks';
 import { getCustomer } from '../../../api/customers/getCustomer';
-import type { Customer } from '@commercetools/platform-sdk';
+import type { Address, Customer } from '@commercetools/platform-sdk';
 import { Loader } from '../Loader/Loader';
+import styles from './Profile.module.css';
+import avatarSrc from '../../../assets/img/avatar.svg';
 
 export function Profile() {
   const [customer, setCustomer] = useState<Customer | undefined>();
@@ -12,17 +14,53 @@ export function Profile() {
     if (!customerId) throw new Error('customerId is null');
     const customerData: Promise<Customer> = getCustomer(customerId);
     customerData.then((data) => {
-      console.log(data);
       setCustomer(data);
     });
   }, [customerId]);
 
-  return !customer ? (
+  const shippingAddress = customer?.addresses.find((address: Address) => {
+    if (customer.shippingAddressIds) return address.id === customer.shippingAddressIds[0];
+    return false;
+  });
+  const billingAddress = customer?.addresses.find((address: Address) => {
+    if (customer.billingAddressIds) return address.id === customer.billingAddressIds[0];
+    return false;
+  });
+
+  return !customer || !shippingAddress || !billingAddress ? (
     <Loader />
   ) : (
-    <>
-      {customerId && <p>customer: {customer.firstName}</p>}
-      <h1>Профиль</h1>
-    </>
+    <div className={styles.profile}>
+      <div className={styles.profile_wrapper}>
+        <h1>Профиль</h1>
+        <img src={avatarSrc} alt="avatar" className={styles.avatar} />
+        <section className={styles.personal_data}>
+          <p>Имя: {customer.firstName}</p>
+          <p>Фамилия: {customer.lastName}</p>
+          <p>Email: {customer.email}</p>
+          <p>Дата рождения: {customer.dateOfBirth}</p>
+        </section>
+
+        <section className={styles.addresses_data}>
+          <fieldset className={styles.address_data}>
+            <legend className={styles.legend}>Адрес доставки</legend>
+            <p>Страна: {shippingAddress.country === 'RU' ? 'Россия' : 'Беларусь'}</p>
+            <p>Город: {shippingAddress.city}</p>
+            <p>Улица: {shippingAddress.streetName}</p>
+            <p>Почтовый индекс: {shippingAddress.postalCode}</p>
+            {customer.defaultShippingAddressId && <p className={styles.address_label}>Адрес по умолчанию</p>}
+          </fieldset>
+
+          <fieldset className={styles.address_data}>
+            <legend className={styles.legend}>Адрес выставления счёта</legend>
+            <p>Страна: {billingAddress.country === 'RU' ? 'Россия' : 'Беларусь'}</p>
+            <p>Город: {billingAddress.city}</p>
+            <p>Улица: {billingAddress.streetName}</p>
+            <p>Почтовый индекс: {billingAddress.postalCode}</p>
+            {customer.defaultBillingAddressId && <p className={styles.address_label}>Адрес по умолчанию</p>}
+          </fieldset>
+        </section>
+      </div>
+    </div>
   );
 }
