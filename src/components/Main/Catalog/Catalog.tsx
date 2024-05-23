e166dac9226a967403d28e3145e6fe740827a5eb
ValeriiaKuz <lerabukareva@icloud.com>
2024-05-23 20:55:58 +0300

    feat: add request into catalog

--- src/components/Main/Catalog/Catalog.tsx
+++ src/components/Main/Catalog/Catalog.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+import { getProducts } from '../../../api/products/getProducts';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import { Loader } from '../Loader/Loader';
+
 export function Catalog() {
-  return (
+  const dispatch = useAppDispatch();
+  const { isProductsLoading } = useAppSelector((state) => state.products_slice);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return isProductsLoading ? (
+    <Loader />
+  ) : (
     <>
       <h1>Каталог</h1>
     </>
