9b687b9702b47b56673eaab9534a9f9ce76b5370
ValeriiaKuz <lerabukareva@icloud.com>
2024-05-30 23:52:28 +0300

    feat: change request for products

--- src/store/slices/products-slice.ts
+++ src/store/slices/products-slice.ts
@@ -1,24 +1,16 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-import type { Product, ProductData } from '@commercetools/platform-sdk';
-import {
-  getFilteredProducts,
-  getPlanetProducts,
-  getSubcategoryProducts
-} from '../../api/products/getProducts';
+import type { ProductProjection } from '@commercetools/platform-sdk';
+import { getProducts } from '../../api/products/getProducts';
 
 export interface ProductsSliceState {
   isProductsLoading: boolean;
-  isFilteredLoading: boolean;
-  products: ProductData[];
-  filteredProducts: ProductData[];
+  products: ProductProjection[];
 }
 
 const initialState: ProductsSliceState = {
   isProductsLoading: false,
-  isFilteredLoading: false,
-  products: [],
-  filteredProducts: []
+  products: []
 };
 export const productsSlice = createSlice({
   name: 'products_slice',
@@ -26,41 +18,23 @@ export const productsSlice = createSlice({
   reducers: {
     deleteProducts(state: ProductsSliceState) {
       state.products = [];
-    },
-    deleteFilteredProducts(state) {
-      state.filteredProducts = [];
     }
   },
   extraReducers: (builder) => {
     const setLoading = (state: ProductsSliceState, isLoading: boolean) => {
       state.isProductsLoading = isLoading;
     };
-    const setFilteredLoading = (state: ProductsSliceState, isLoading: boolean) => {
-      state.isFilteredLoading = isLoading;
-    };
 
-    const setProducts = (state: ProductsSliceState, action: PayloadAction<Product[]>) => {
-      state.products = action.payload.map((product) => product.masterData.current);
-      state.filteredProducts = [];
+    const setProducts = (state: ProductsSliceState, action: PayloadAction<ProductProjection[]>) => {
+      state.products = action.payload.map((product) => product);
       state.isProductsLoading = false;
-    };
-    const setFilteredProducts = (state: ProductsSliceState, action: PayloadAction<Product[]>) => {
-      state.filteredProducts = action.payload.map((product) => product.masterData.current);
-      state.isFilteredLoading = false;
     };
 
     builder
-      .addCase(getPlanetProducts.pending, (state) => setLoading(state, true))
-      .addCase(getPlanetProducts.fulfilled, (state, action) => setProducts(state, action))
-      .addCase(getPlanetProducts.rejected, (state) => setLoading(state, false))
-      .addCase(getSubcategoryProducts.pending, (state) => setLoading(state, true))
-      .addCase(getSubcategoryProducts.fulfilled, (state, action) => setProducts(state, action))
-      .addCase(getSubcategoryProducts.rejected, (state) => setLoading(state, false))
-      .addCase(getFilteredProducts.pending, (state) => setFilteredLoading(state, true))
-      .addCase(getFilteredProducts.fulfilled, (state, action) => setFilteredProducts(state, action))
-      .addCase(getFilteredProducts.rejected, (state) => setFilteredLoading(state, false));
+      .addCase(getProducts.pending, (state) => setLoading(state, true))
+      .addCase(getProducts.fulfilled, (state, action) => setProducts(state, action))
+      .addCase(getProducts.rejected, (state) => setLoading(state, false));
   }
 });
 
 export const deleteProducts = productsSlice.actions.deleteProducts;
-export const deleteFilteredProducts = productsSlice.actions.deleteFilteredProducts;
