a4b1dc66c205467b18aa6b74c5aaafb46920126c
Natalia Bandurova <natacsp@mail.ru>
2024-05-30 09:53:36 +0300

    fix: fix logaut order

--- src/api/customers/logoutCustomer.ts
+++ src/api/customers/logoutCustomer.ts
@@ -3,7 +3,7 @@ import { deleteCustomer } from '../../store/slices/customer-slice';
 import { defaultPlanet, setPlanet } from '../../store/slices/planet-slice';
 
 export const logout = (dispatch: AppDispatch) => {
+  dispatch(setPlanet(defaultPlanet));
   localStorage.clear();
   dispatch(deleteCustomer());
-  dispatch(setPlanet(defaultPlanet));
 };
