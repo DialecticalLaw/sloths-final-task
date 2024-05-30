4a8afa03948a3deeb4d7111db12e4f0bf839a13a
Natalia Bandurova <natacsp@mail.ru>
2024-05-30 14:50:56 +0300

    feat: add close button

--- src/components/Main/ProductDetail/ImageModal/ImageModal.tsx
+++ src/components/Main/ProductDetail/ImageModal/ImageModal.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styles from './ImageModal.module.css';
+import { Button } from '../../../univComponents/Button/Button';
 
 interface ImageModalProps {
   imageUrl: string;
@@ -13,19 +14,21 @@ export function ImageModal({ imageUrl, onClose }: ImageModalProps) {
         onClose();
       }
     };
+    document.body.classList.add('no-scroll');
     window.addEventListener('keydown', handleEsc);
     return () => {
       window.removeEventListener('keydown', handleEsc);
+      document.body.classList.remove('no-scroll');
     };
   }, [onClose]);
 
   return (
     <div className={styles.modal_overlay} onClick={onClose}>
       <div className={styles.modal_content} onClick={(e) => e.stopPropagation()}>
-        <button className={styles.close_button} onClick={onClose}>
-          &times;
-        </button>
         <img src={imageUrl} alt="Enlarged" className={styles.enlarged_image} />
+        <Button onClick={onClose} type="button">
+          Закрыть
+        </Button>
       </div>
     </div>
   );
