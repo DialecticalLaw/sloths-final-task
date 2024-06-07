87f931f46520ed4289e02af4991c0c996c16f437
DialecticalLaw <d.shmuratkin@mail.ru>
2024-06-07 23:00:07 +0300

    feat: improve products design

--- src/components/Main/Cart/Cart.tsx
+++ src/components/Main/Cart/Cart.tsx
@@ -6,10 +6,11 @@ import { formatPrice } from '../../../helpers/formatPrice';
 
 export function Cart() {
   const cart = useAppSelector((state) => state.cart_slice.cart);
+  console.log(cart?.lineItems);
 
   return (
     <section>
-      <h1>Корзина</h1>
+      <h1 className={styles.title}>Корзина</h1>
       {cart ? (
         <div className={styles.products_wrapper}>
           {cart.lineItems.map((item: LineItem, index) => {
@@ -18,12 +19,47 @@ export function Cart() {
             const bgImageUrl = item.variant?.images?.length ? item.variant.images[0].url : '';
 
             return (
-              <div className={styles.product} key={index}>
-                <div className={styles.product_image} style={{ backgroundImage: `url(${bgImageUrl})` }} />
-                <p>{item.name.ru}</p>
-                <Price classes={[styles.product_price_wrapper]} price={price} discountPrice={discountPrice} />
-                <p>Итого: ${formatPrice(item.quantity * (discountPrice || price))}</p>
-              </div>
+              <table className={styles.product} key={index}>
+                <thead>
+                  <tr>
+                    <th className={styles.product_prop}>Продукт</th>
+                    <th className={styles.product_prop}>Цена</th>
+                    <th className={styles.product_prop}>Количество</th>
+                    <th className={styles.product_prop}>Итого</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className={styles.product_main_info_cell}>
+                      <div className={styles.product_image_wrapper}>
+                        <div
+                          className={styles.product_image}
+                          style={{ backgroundImage: `url(${bgImageUrl})` }}
+                        />
+                        <div className={styles.product_image_glow_wrapper}>
+                          <img src={bgImageUrl} alt="glow" className={styles.product_image_glow} />
+                        </div>
+                      </div>
+                      <p className={`${styles.product_name} ${discountPrice && styles.discount_name}`}>
+                        {item.name.ru}
+                      </p>
+                    </td>
+                    <td>
+                      <Price
+                        classes={[styles.product_price_wrapper]}
+                        price={price}
+                        discountPrice={discountPrice}
+                      />
+                    </td>
+                    <td>+ 1 -</td>
+                    <td>
+                      <p className={styles.total}>
+                        Итого: {formatPrice(item.quantity * (discountPrice || price))}
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             );
           })}
         </div>
