316a8cd7a136e6dbc0c705445eeefb6060ef06f8
DialecticalLaw <d.shmuratkin@mail.ru>
2024-06-07 14:11:35 +0300

    feat: add an image of the product to the card in the cart

--- src/components/Main/Cart/Cart.tsx
+++ src/components/Main/Cart/Cart.tsx
@@ -15,9 +15,11 @@ export function Cart() {
           {cart.lineItems.map((item: LineItem, index) => {
             const price = item.price.value.centAmount;
             const discountPrice = item.price.discounted?.value.centAmount;
+            const bgImageUrl = item.variant?.images?.length ? item.variant.images[0].url : '';
 
             return (
               <div className={styles.product} key={index}>
+                <div className={styles.product_image} style={{ backgroundImage: `url(${bgImageUrl})` }} />
                 <p>{item.name.ru}</p>
                 <Price classes={[styles.product_price_wrapper]} price={price} discountPrice={discountPrice} />
               </div>
