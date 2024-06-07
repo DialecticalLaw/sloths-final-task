b67e75092b35c458360eaf1993677e2299c69b2e
Natalia Bandurova <natacsp@mail.ru>
2024-06-07 04:33:51 +0300

    feat: save customers cart after logout

--- src/components/Header/CartIcon/CartIcon.tsx
+++ src/components/Header/CartIcon/CartIcon.tsx
@@ -2,16 +2,38 @@ import cartIcon from '../../../assets/img/basket.png';
 import emptyCartIcon from '../../../assets/img/emptyBasket.png';
 import styles from './CartIcon.module.css';
 import { NavLink } from 'react-router-dom';
-import { useAppSelector } from '../../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 import type { CustomNavLinkProps } from '../Header.interfaces';
+import { useEffect } from 'react';
+import { getCart } from '../../../api/cart/createCart';
+import { setCart } from '../../../store/slices/cart-slice';
 
 export function CartIcon({ toggleMenuOpen }: CustomNavLinkProps) {
   const cart = useAppSelector((state) => state.cart_slice.cart);
+  const customerId = useAppSelector((state) => state.customer_slice.customerId);
+  const dispatch = useAppDispatch();
+
   let totalLineItemQuantity;
 
   if (cart) {
     totalLineItemQuantity = cart.totalLineItemQuantity;
   }
+
+  useEffect(() => {
+    const fetchCart = async () => {
+      if (customerId) {
+        try {
+          const response = await getCart(customerId);
+          const cart = response.body;
+          dispatch(setCart(cart));
+        } catch (error) {
+          console.error('Error fetching cart:', error);
+        }
+      }
+    };
+
+    fetchCart();
+  }, [customerId, dispatch]);
 
   return (
     <NavLink to={'/cart'} className={styles.cart_link}>
