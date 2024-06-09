d8108b9b9b1790f19a94950ca098bb581d0eb738
ValeriiaKuz <lerabukareva@icloud.com>
2024-06-09 16:48:49 +0300

    refactor: add planets into array and use map for render

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -16,6 +16,13 @@ export function Sidebar() {
   const { planet } = useAppSelector((state) => state.planet_slice);
   const [isSidebarVisible, setVisibility] = useState<boolean>(false);
   const navigation = useNavigate();
+
+  const planets = [
+    { value: Planets.mars, label: '_Марс', className: 'mars' },
+    { value: Planets.venus, label: '_Венера', className: 'venus' },
+    { value: Planets.earth, label: '_Земля', className: 'earth' }
+  ];
+
   const onPlanetClick: MouseEventHandler<HTMLInputElement> = (e) => {
     if (e.target instanceof HTMLInputElement) {
       const value = e.target.value as Planets;
@@ -25,6 +32,10 @@ export function Sidebar() {
       dispatch(setFilter(null));
     }
   };
+  const onSidebarHandlerClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    setVisibility(!isSidebarVisible);
+  };
 
   return (
     <>
@@ -32,58 +43,28 @@ export function Sidebar() {
       <aside className={styles.sidebar + ' ' + (isSidebarVisible ? styles.sidebar__visible : '')}>
         <form className={styles.form}>
           <div className={styles.planet_list}>
-            <div className={styles.catalog_item}>
-              <label className={styles.planet_item}>
-                <input
-                  type="radio"
-                  name="picked"
-                  value={Planets.mars}
-                  className={styles.planet + ' ' + styles.mars}
-                  defaultChecked={planet === Planets.mars}
-                  onClick={onPlanetClick}
-                />
-                _Марс
-              </label>
-              {planet === Planets.mars && isShowSubcategory && (
-                <SubcategoriesList setVisibility={setVisibility} />
-              )}
-            </div>
-            <div className={styles.catalog_item}>
-              <label className={styles.planet_item}>
-                <input
-                  type="radio"
-                  name="picked"
-                  value={Planets.venus}
-                  className={styles.planet + ' ' + styles.venus}
-                  defaultChecked={planet === Planets.venus}
-                  onClick={onPlanetClick}
-                />
-                _Венера
-              </label>
-              {planet === Planets.venus && isShowSubcategory && (
-                <SubcategoriesList setVisibility={setVisibility} />
-              )}
-            </div>
-            <div className={styles.catalog_item}>
-              <label className={styles.planet_item}>
-                <input
-                  type="radio"
-                  name="picked"
-                  value={Planets.earth}
-                  defaultChecked={planet === Planets.earth}
-                  className={styles.planet + ' ' + styles.earth}
-                  onClick={onPlanetClick}
-                />
-                _Земля
-              </label>
-              {planet === Planets.earth && isShowSubcategory && (
-                <SubcategoriesList setVisibility={setVisibility} />
-              )}
-            </div>
+            {planets.map((planetItem) => (
+              <div className={styles.catalog_item} key={planetItem.value}>
+                <label className={styles.planet_item}>
+                  <input
+                    type="radio"
+                    name="picked"
+                    value={planetItem.value}
+                    className={`${styles.planet} ${styles[planetItem.className]}`}
+                    defaultChecked={planetItem.value === planet}
+                    onClick={onPlanetClick}
+                  />
+                  {planetItem.label}
+                </label>
+                {planetItem.value === planet && isShowSubcategory && (
+                  <SubcategoriesList setVisibility={setVisibility} />
+                )}
+              </div>
+            ))}
           </div>
-          <div onClick={() => setVisibility(!isSidebarVisible)} className={styles.arrow_wrapper}>
+          <button onClick={onSidebarHandlerClick} className={styles.arrow_wrapper}>
             <img src={arrowIcon} alt="arrow" className={styles.arrow} />
-          </div>
+          </button>
         </form>
       </aside>
     </>
