c24c6e47324164a4369f7e54369d24993d06bdd7
ValeriiaKuz <lerabukareva@icloud.com>
2024-06-10 08:20:11 +0300

    refactor: add the configuration of the planets and the handler for less code duplication

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,38 +1,21 @@
 import styles from './Sidebar.module.css';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { setPlanet, Planets } from '../../store/slices/planet-slice';
+import { useLocation } from 'react-router-dom';
+import { useAppSelector } from '../../store/hooks';
 import type { MouseEventHandler } from 'react';
 import { useState } from 'react';
 import arrowIcon from '../../assets/img/arrow.svg';
 import { BgPlanets } from './Bg-planets';
 import { SubcategoriesList } from './Subcategories/Subcategories';
-import { setFilter } from '../../store/slices/products-slice';
+import { planetsConfig, usePlanetClickHandler } from '../../helpers/planetsConfig';
 
 export function Sidebar() {
-  const dispatch = useAppDispatch();
   const locationPath = useLocation().pathname;
   const isShowSubcategory = locationPath.startsWith('/catalog');
   const { planet } = useAppSelector((state) => state.planet_slice);
   const [isSidebarVisible, setVisibility] = useState<boolean>(false);
-  const navigation = useNavigate();
 
-  const planets = [
-    { value: Planets.mars, label: '_Марс', className: 'mars' },
-    { value: Planets.venus, label: '_Венера', className: 'venus' },
-    { value: Planets.earth, label: '_Земля', className: 'earth' }
-  ];
+  const onPlanetClick = usePlanetClickHandler();
 
-  const onPlanetClick: MouseEventHandler<HTMLInputElement> = (e) => {
-    if (e.target instanceof HTMLInputElement) {
-      const value = e.target.value as Planets;
-      setVisibility(false);
-      localStorage.setItem('sloth-selectedPlanet', value);
-      dispatch(setPlanet(value));
-      navigation(`/catalog/${value}`);
-      dispatch(setFilter(null));
-    }
-  };
   const onSidebarHandlerClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
     setVisibility(!isSidebarVisible);
@@ -44,7 +27,7 @@ export function Sidebar() {
       <aside className={styles.sidebar + ' ' + (isSidebarVisible ? styles.sidebar__visible : '')}>
         <form className={styles.form}>
           <div className={styles.planet_list}>
-            {planets.map((planetItem) => (
+            {planetsConfig.map((planetItem) => (
               <div className={styles.catalog_item} key={planetItem.value}>
                 <label className={styles.planet_item}>
                   <input
@@ -53,7 +36,10 @@ export function Sidebar() {
                     value={planetItem.value}
                     className={`${styles.planet} ${styles[planetItem.className]}`}
                     defaultChecked={planetItem.value === planet}
-                    onClick={onPlanetClick}
+                    onClick={(e) => {
+                      onPlanetClick(e);
+                      setVisibility(false);
+                    }}
                   />
                   {planetItem.label}
                 </label>
