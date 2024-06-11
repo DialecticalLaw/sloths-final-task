3e5c5a5954f3058148786c78ff7ec3cc77233c2a
ValeriiaKuz <lerabukareva@icloud.com>
2024-06-11 09:10:30 +0300

    feat: add bg and members to About page

--- src/components/Main/About/About.tsx
+++ src/components/Main/About/About.tsx
@@ -1,13 +1,19 @@
-import { BgPlanets } from '../../Sidebar/Bg-planets';
-import { useAppSelector } from '../../../store/hooks';
 import styles from './About.module.css';
+import spaceman from '../../../assets/img/spaceman.png';
 
 export function About() {
-  const planet = useAppSelector((state) => state.planet_slice.planet);
+  const astronauts = ['Валерия', 'Денис', 'Наталья'];
   return (
-    <section className={styles.home_page}>
-      {planet && <BgPlanets />}
-      <h1>О нас</h1>
+    <section className={styles.bg}>
+      <ul className={styles.people}>
+        {astronauts.map((astronaut) => {
+          return (
+            <li className={styles.astronaut} key={astronaut}>
+              <img src={spaceman} alt={astronaut} />
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
