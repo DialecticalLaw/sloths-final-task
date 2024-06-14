106323f933fb9a288473e0d67c3c926685f13e1d
DialecticalLaw <d.shmuratkin@mail.ru>
2024-06-14 18:30:24 +0300

    feat: add a promo code input field

--- src/components/Main/Cart/CartSummary/CartSummary.tsx
+++ src/components/Main/Cart/CartSummary/CartSummary.tsx
@@ -1,12 +1,41 @@
 import type { Cart } from '@commercetools/platform-sdk';
 import styles from './CartSummary.module.css';
 import { formatPrice } from '../../../../helpers/formatPrice';
+import { Button } from '../../../univComponents/Button/Button';
+import voucherIcon from '../../../../assets/img/voucher.svg';
+import { useState } from 'react';
 
 export function CartSummary({ cart }: { cart: Cart }) {
+  const [promoCode, setPromoCode] = useState('');
+
   return (
     <div className={styles.cart_total}>
       <p>Цена товаров в корзине: {formatPrice(cart.totalPrice.centAmount)}</p>
       <p>Общее количество: {cart.totalLineItemQuantity}</p>
+
+      <form className={styles.promo_code}>
+        <img className={styles.voucher_icon} src={voucherIcon} alt="voucher" />
+        <label className={styles.label}>
+          <input
+            className={styles.input}
+            type={'text'}
+            placeholder={'Промокод'}
+            value={promoCode}
+            onChange={(e) => setPromoCode(e.target.value)}
+          />
+          <div className={styles.line} />
+        </label>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            console.log(promoCode);
+          }}
+          minimal
+          type="submit"
+        >
+          Применить
+        </Button>
+      </form>
     </div>
   );
 }
