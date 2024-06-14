d5a29990e4d2774a1b035f4dc89f464a3de533ca
DialecticalLaw <d.shmuratkin@mail.ru>
2024-06-14 11:58:34 +0300

    feat: change subtitle

--- src/components/HorizontalSidebar/HorizontalSidebar.tsx
+++ src/components/HorizontalSidebar/HorizontalSidebar.tsx
@@ -7,7 +7,9 @@ export function HorizontalSidebar() {
 
   return (
     <div className={styles.sidebar}>
-      <h2 className={styles.subtitle}>Загляните в каталог - найдётся всё, что нужно для счастья.</h2>
+      <h2 className={styles.subtitle}>
+        Погрузитесь в космический каталог - найдётся всё, о чём мечтаете среди звёзд.
+      </h2>
       <div className={styles.planet_list}>
         {planetsConfig.map((planetItem) => (
           <Planet key={planetItem.value} planetItem={planetItem} onClick={onPlanetClick} />
